31610baac0dea2a7f392fa1f79ea655bcbe147da
Logan <sir.roberts@disroot.org>
2019-10-10 00:01:15 +0200

    números negativos

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 /* Tareas pendientes:
 
-# números negativos
 # poder realizar operaciones a través del teclado también
+# que no se pueda poner un signo de multiplicación/división detrás de un + o un -
+# que no se pueda poner un + o un - después de dos símbolos de sumar y/o restar
 
 */
 
 const datos = document.getElementById('datos');
-let currentValue = '';
-let lastValue = '';
+let currentValue = null;
+let lastValue = null;
+//let aCalcular = [];
+//let sumasRestas = [];
 
 function clickHandler (event) {
   currentValue = event.target.value;
-  console.log(lastValue,currentValue);
 
   if (lastValue === '/' && currentValue == 0) {
     alert('¡No se puede dividir entre cero!');
   } else {
     datos.innerText += currentValue;
   }
+
+  /*switch (currentValue) {
+    case '+': if (lastValue && !isNaN(lastValue)) { sumasRestas.push(currentValue) }; break;
+    case '-': if (lastValue && !isNaN(lastValue)) { sumasRestas.push(currentValue) }; break;
+  }*/
 
   lastValue = currentValue;
 }
@@ -28,18 +35,29 @@ function reiniciar () {
 
 function calcular () {
   const input = datos.innerText;
+                                              // n: negativo
+  let valores = input.replace(/\+\+/g,'+')    // ++ => +
+                     .replace(/\-\-/g,'-n')   // -- => -n
+                     .replace(/\+\-/g,'+n')   // +- => +n
+                     .replace(/\-\+/g,'-')    // -+ => -
+                     .replace(/\/\+/g,'/')    // /+ => /
+                     .replace(/x\+/g,'x')     // x+ => x
+                     .replace(/\/-/g,'\/n')   // /- => /n
+                     .replace(/x-/g,'xn');    // x- => xn
   // nos quedamos por un lado las sumas y las restas
-  let sumasRestas = input.replace(/[\d|x|/]/g,'').split('');
+  let sumasRestas = valores.replace(/[\d|x|n|\/]/g,'').split('');
   // y por otro con las divisiones y las multiplicaciones
   // que calcularemos lo primero
-  let filtro = input.split(/[-|+]/g);
+  let filtro = valores.replace(/[\+|\-]/g,',').split(',');
+  console.log(sumasRestas,filtro);
   let numeros = []; // y vamos a añadir los números resultantes en un array
 
   filtro.forEach( elemento => {
     if (elemento.includes('x') || elemento.includes('/')) {
       // separamos de nuevo las cifras de los operadores
-      let operaciones = elemento.split(/[x|\/]/g);
-      let multiDivisiones = elemento.replace(/\d/g,'').split('');
+      let operaciones = elemento.replace(/n/g,'-').split(/[x|\/]/g);
+      console.log(operaciones);
+      let multiDivisiones = elemento.replace(/[\d|n]/g,'').split('');
       let operacion = 0;
 
       // y calculamos !!
@@ -58,7 +76,7 @@ function calcular () {
       numeros.push(resultado);
 
     } else {
-      numeros.push(parseInt(elemento));
+      numeros.push(elemento.replace('n','-'));
     }
   });
 
